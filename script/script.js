670e52911335fde7f32732cbf4b25f63004869ce
Nicolas Nascimento <devnicolasnasc@gmail.com>
2026-09-26 00:24:45 -0300

    Adiciona animações ao site com GSAP

---
+++ script/script.js
@@ -0,0 +1,16 @@
+gsap.from(".hero", {
+    opacity: 0,
+    y: 50,
+    duration: 4,
+    stagger: 0.2,
+    ease: "power2.out"
+});
+
+gsap.from(".second-hero", {
+    opacity: 0,
+    y: -50,
+    delay: 1,
+    duration: 4,
+    stagger: 0.2,
+    ease: "power2.out"
+});
